7dd415f91715e81ef2934e7a4c3f39e68128d2c8
jtron82 <tranj46@gmail.com>
2020-10-28 16:13:35 -0500

    Add media constraints for self and peer stream templates

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -6,3 +6,39 @@ var sc = io.connect('/' + NAMESPACE);
 sc.on('message', function(data) {
   console.log('Message received: ' + data);
 });
+
+// tracking client states
+var clientIs = {
+  makingOffer: false,
+  ignoringOffer: false,
+  // has opposite 'impolite' 
+  // impolite makes connection accept offer regardless
+  // only one side of connection needs to be polite
+  // polite accepts offer even though it may already have one
+  polite: false
+}
+
+// eventual setup of STUN servers
+var rtcConfig = null;
+// pc = peer connection
+var pc = new RTCPeerConnection(rtcConfig);
+
+// handle video streams
+// setting media constraints for video and audio
+var mediaConstraints = {
+  video: true,
+  audio: false
+};
+
+// handle self video
+var selfVideo = document.querySelector('#self-video');
+var selfStream = new MediaStream();
+// setting self video source object to empty self stream
+selfVideo.srcObject = selfStream;
+
+// handle peer video
+var peerVideo = document.querySelector('#peer-video');
+var peerStream = new MediaStream();
+// setting peer video source object to empty peer stream
+peerVideo.srcObject = peerStream;
+
