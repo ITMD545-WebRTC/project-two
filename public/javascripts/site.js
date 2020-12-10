059cfc6ef06ff7f7d6c9b9f79162f61bc552214a
Dasha Day Hisoler <dashadayhisoler@gmail.com>
2020-12-10 07:14:22 -0600

    Reduce STUN servers to two

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -24,11 +24,8 @@ var rtcConfig = {
   iceServers: [
     {
       urls: [
-        'stun:stun.l.google.com:19302',
         'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302'
+        'stun:stun2.l.google.com:19302'
       ]
     }
   ]
